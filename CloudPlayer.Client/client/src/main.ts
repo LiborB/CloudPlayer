664929e1da1c9b8eab8335732b42bb86c4e6f409
lbicanovsky <lbicanovsky@logtagrecorders.com>
2020-07-08 15:12:04 +1200

    Setting up server to work with nginx

--- CloudPlayer.Client/client/src/main.ts
+++ CloudPlayer.Client/client/src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import Statics from './shared/statics'
 
-if (window.location.hostname === "cloudplayer.liborb.com") {
+if (window.location.hostname.endsWith("cloudplayer.liborb.com")) {
     Statics.baseApiUrl = "/";
 }
 else {
