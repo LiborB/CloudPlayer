9856de172e32662a85ac206d9255dc54219ced81
lbicanovsky <lbicanovsky@logtagrecorders.com>
2020-07-08 15:08:48 +1200

    Setting up server to work with nginx

--- CloudPlayer.Client/client/src/main.ts
+++ CloudPlayer.Client/client/src/main.ts
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Statics from './shared/statics'
+
+if (window.location.hostname === "cloudplayer.liborb.com") {
+    Statics.baseApiUrl = "/";
+}
+else {
+    Statics.baseApiUrl = "https://localhost:5001/api/";
+}
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
---
+++ CloudPlayer.Client/client/src/shared/statics.ts
@@ -0,0 +1,3 @@
+export default class Statics {
+    public static baseApiUrl: string = "";
+}
